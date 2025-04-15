3579e994016f41e4c21c720610e18fbd61d7523c
Courts02 <courtneycook2006@gmail.com>
2025-04-15 15:17:20 +0200

    Node.JS

---
+++ Unit 0/Lesson 2/quickCheck.js
@@ -0,0 +1,4 @@
+// Quick check 2.2 
+// 1 What are three ways in which you could exit the REPL environment?
+//  2 How do you load a file that isn’t in your project folder into REPL?
+//  3 What happens if you run .save with a filename that already exists?
